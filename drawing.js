c87b7bb21828b8c9291d91398f0b86c300ed62b0
xWacob <89878961+xWacob@users.noreply.github.com>
2023-08-10 18:28:22 -0400

    Add html boilerplate and start grid logic
    The grid currently is just making the divs and not putting them in any
    flex type of form.

---
+++ drawing.js
@@ -0,0 +1,33 @@
+/*
+    Author: Jacob Williams
+    Project: TheOdinProject Etch A Sketch
+
+    Code Credits (for help):
+*/
+
+// **************** Global Vars ****************************************
+
+let grid_size = 4;
+
+// ********************** Main function area ***************************
+
+
+
+// *********************************************************************
+
+// ************************ Functions **********************************
+
+function createGrid()
+{
+    const container = document.getElementById("squares-container");
+    for (let i = 0; i < grid_size; i++)
+    {
+        const rows = document.createElement("div");
+        container.appendChild(rows);
+        for (let i = 0; i < grid_size; i++)
+        {
+            const tile = document.createElement("div");
+            rows.appendChild(tile);
+        }
+    }
+}
