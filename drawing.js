91288214e6033d2671f5c56c088882658fcef400
xWacob <89878961+xWacob@users.noreply.github.com>
2023-08-17 18:28:28 -0400

    Add divs to start grid creation

--- drawing.js
+++ drawing.js
@@ -7,17 +7,33 @@
 
 // **************** Global Vars ****************************************
 
-const DEFAULT_SIZE = 16;
+let gridSize = 16;
 const DEFAULT_BGCOLOR = "FEFEFE";
 // ********************** Main function area ***************************
 
-
+createGrid(gridSize);
 
 // *********************************************************************
 
 // ************************ Functions **********************************
 
-function createGrid(size)
+function createGrid(size) // this will be used in the creation of the tiles which have the listeners
 {
+    const container = document.getElementById("squares-container");
 
+    for (let i = 0; i < size; i++)
+    {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        container.appendChild(row);
+        console.log("row loop");
+
+        for(let j = 0; j < size; j++) // size x size grid
+        {
+            const tile = document.createElement("div");
+            tile.classList.add("tile");
+            tile.style.backgroundColor = DEFAULT_BGCOLOR;
+            row.appendChild(tile);
+        }
+    }
 }
