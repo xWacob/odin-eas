fb98f0ad9652298ba9267dc0172034eb72f97983
xWacob <89878961+xWacob@users.noreply.github.com>
2023-08-16 20:37:26 -0400

    Make black squares turn white when rehovered
    It is still in progress but I wanted to try something to fix it so I want
    to restore just in case

--- drawing.js
+++ drawing.js
@@ -8,18 +8,64 @@
 // **************** Global Vars ****************************************
 
 let grid_size = 16;
+let gridCreated = 1; // 1 means no grid, 0 means grid created
 
 // ********************** Main function area ***************************
 
-createGrid();
+
 const tileArr = document.getElementsByClassName("tile");
-for (let i = 0; i < tileArr.length; i++)
+const coloredArr = [];
+const cursorArr = [];
+
+
+if (gridCreated == 1) // create grid and array to see if tile has been entered
 {
-    Object.values(tileArr)[i].addEventListener("mouseenter", e => {
-        Object.values(tileArr)[i].style.backgroundColor = "black";
-    })
+    createGrid();
+    for (let i = 0; i < tileArr.length; i++)
+    {
+        coloredArr.push(false);
+        cursorArr.push(false);
+    }
 }
 
+
+for (let i = 0; i < tileArr.length; i++)
+{
+
+    Object.values(tileArr)[i].addEventListener("mouseenter", () => {
+        cursorArr[i] = true; 
+        if(Object.values(tileArr)[i].style.backgroundColor == "#FFFFFF")
+        {
+            coloredArr[i] = true;
+            console.log("if state");
+        }
+
+        let color = getComputedStyle(Object.values(tileArr)[i], "backgroundColor");
+        console.log(`black ${color}`)
+    });
+
+    Object.values(tileArr)[i].addEventListener("mouseleave", () => {
+        cursorArr[i] = false;
+
+        if (Object.values(tileArr)[i].style.backgroundColor === "#000000")
+        {
+            coloredArr[i] = false;
+        }
+        let color = getComputedStyle(Object.values(tileArr)[i], "backgroundColor");
+        console.log(`white ${color}`);
+    });
+
+    if(cursorArr[i] && coloredArr[i] && Object.values(tileArr)[i].style.backgroundColor === "#FFFFFF")
+    {
+        Object.values(tileArr)[i].style.backgroundColor = "black";
+    }
+
+    if(cursorArr[i] && !coloredArr[i] && Object.values(tileArr)[i].style.backgroundColor === "#000000")
+    {
+        Object.values(tileArr)[i].style.backgroundColor = "white";
+    }
+       
+}
 
 
 // *********************************************************************
