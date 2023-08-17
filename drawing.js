94c1df32f193efff371c9e56232a3b7d612af75f
xWacob <89878961+xWacob@users.noreply.github.com>
2023-08-17 12:34:22 -0400

    Refactor whole code structure
    Basically got rid of all lines since the code was messy. Going to look into
    better options to do the code from the beginning.

--- drawing.js
+++ drawing.js
@@ -7,84 +7,17 @@
 
 // **************** Global Vars ****************************************
 
-let grid_size = 16;
-let gridCreated = 1; // 1 means no grid, 0 means grid created
-
+const DEFAULT_SIZE = 16;
+const DEFAULT_BGCOLOR = "FEFEFE";
 // ********************** Main function area ***************************
 
-
-const tileArr = document.getElementsByClassName("tile");
-const coloredArr = [];
-const cursorArr = [];
-
-
-if (gridCreated == 1) // create grid and array to see if tile has been entered
-{
-    createGrid();
-    for (let i = 0; i < tileArr.length; i++)
-    {
-        coloredArr.push(false);
-        cursorArr.push(false);
-    }
-}
-
-
-for (let i = 0; i < tileArr.length; i++)
-{
-
-    Object.values(tileArr)[i].addEventListener("mouseenter", () => {
-        cursorArr[i] = true; 
-        if(Object.values(tileArr)[i].style.backgroundColor == "#FFFFFF")
-        {
-            coloredArr[i] = true;
-            console.log("if state");
-        }
-
-        let color = getComputedStyle(Object.values(tileArr)[i], "backgroundColor");
-        console.log(`black ${color}`)
-    });
-
-    Object.values(tileArr)[i].addEventListener("mouseleave", () => {
-        cursorArr[i] = false;
-
-        if (Object.values(tileArr)[i].style.backgroundColor === "#000000")
-        {
-            coloredArr[i] = false;
-        }
-        let color = getComputedStyle(Object.values(tileArr)[i], "backgroundColor");
-        console.log(`white ${color}`);
-    });
-
-    if(cursorArr[i] && coloredArr[i] && Object.values(tileArr)[i].style.backgroundColor === "#FFFFFF")
-    {
-        Object.values(tileArr)[i].style.backgroundColor = "black";
-    }
-
-    if(cursorArr[i] && !coloredArr[i] && Object.values(tileArr)[i].style.backgroundColor === "#000000")
-    {
-        Object.values(tileArr)[i].style.backgroundColor = "white";
-    }
-       
-}
 
 
 // *********************************************************************
 
 // ************************ Functions **********************************
 
-function createGrid()
+function createGrid(size)
 {
-    const container = document.getElementById("squares-container");
-    for (let i = 0; i < grid_size; i++)
-    {
-        const rows = document.createElement("div");
-        rows.className = "row";
-        container.appendChild(rows);
-        for (let i = 0; i < grid_size; i++)
-        {
-            const tile = document.createElement("div");
-            tile.className = "tile";
-            rows.appendChild(tile);
-        }
-    }
+
 }
