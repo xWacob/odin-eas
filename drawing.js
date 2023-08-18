2b0dd94b233050e4e208e7c9af56f40f3c296efc
xWacob <89878961+xWacob@users.noreply.github.com>
2023-08-17 21:09:09 -0400

    Made grid able to be drawn and erased on
    Grid now can be drawn on and can be erased when the eraser button is toggled

--- drawing.js
+++ drawing.js
@@ -2,16 +2,23 @@
     Author: Jacob Williams
     Project: TheOdinProject Etch A Sketch
 
-    Code Credits (for help):
+    Code Credits (for help): Lines 14, 17, and 18 were adapted from michalosman's code
 */
 
 // **************** Global Vars ****************************************
 
-let gridSize = 16;
-const DEFAULT_BGCOLOR = "FEFEFE";
+let gridSize = 20;
+const GRID_HEIGHT = 720;
+const DEFAULT_BGCOLOR = "#FEFEFE";
+let eraserOn = false;
+let click = false;
 // ********************** Main function area ***************************
 
+document.body.onmousedown = () => {click = true;}
+document.body.onmouseup = () => {click = false;}
+
 createGrid(gridSize);
+eraserButtonCreator();
 
 // *********************************************************************
 
@@ -20,6 +27,8 @@ createGrid(gridSize);
 function createGrid(size) // this will be used in the creation of the tiles which have the listeners
 {
     const container = document.getElementById("squares-container");
+    container.style.height = `${(gridSize * 2) + GRID_HEIGHT}px`;
+    let tileCounter = 0;
 
     for (let i = 0; i < size; i++)
     {
@@ -31,9 +40,60 @@ function createGrid(size) // this will be used in the creation of the tiles whic
         for(let j = 0; j < size; j++) // size x size grid
         {
             const tile = document.createElement("div");
-            tile.classList.add("tile");
+            tile.classList.add(`tile${tileCounter}`);
             tile.style.backgroundColor = DEFAULT_BGCOLOR;
+            tile.style.height = `${GRID_HEIGHT/gridSize}px`;
+            tile.style.flex = 1;
+            tile.style.flexBasis = "auto";
+            tile.style.border = "1px solid black";
+
+            tile.addEventListener("mouseover", changeColor)
+            tile.addEventListener("mousedown", changeColor);
+
             row.appendChild(tile);
+
+            tileCounter++;
         }
     }
+}
+
+function eraserButtonCreator()
+{
+    const ersBtn = document.getElementById("eraser");
+    ersBtn.addEventListener("click", turnOnEraser);
+}
+
+function changeColor(e)
+{
+    if(e.type === "mouseover" && !click)
+        return;
+
+    if(!eraserOn)
+    {
+        e.target.style.backgroundColor = "black";
+    }
+
+    if(eraserOn)
+    {
+        e.target.style.backgroundColor = DEFAULT_BGCOLOR;
+    }
+    //console.log(e.type);
+}
+
+function turnOnEraser(e)
+{
+    //console.log(e.type);
+    if (!eraserOn)
+    {
+        const ersBtn = document.getElementById("eraser");
+        eraserOn = true;
+        ersBtn.style.backgroundColor = "yellow";
+    }
+    else
+    {
+        const ersBtn = document.getElementById("eraser");
+        eraserOn = false;
+        ersBtn.style.backgroundColor = "lightgray";
+    }
+    //console.log(eraserOn);
 }
